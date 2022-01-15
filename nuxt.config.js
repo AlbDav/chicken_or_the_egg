d53e67393add7e7dda3413388b48f8867e0645e7
Davide Albano <davidealbano96@gmail.com>
2022-01-15 20:30:01 +0100

    started adding gsap

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,9 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+	  transpile: ['gsap']
+  },
 
   tailwindcss: {
     config: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,53 @@
 <template>
-  <ChallengeContainer />
+  <div class="bg-black">
+    <div class="h-screen w-full"></div>
+    <div ref="section2" class="h-screen w-full bg-white">ciao</div>
+    <div class="h-screen w-full"></div>
+  </div>
 </template>
 
 <script>
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
 export default {
   name: 'IndexPage',
+  mounted() {
+    gsap.registerPlugin(ScrollTrigger)
+
+    const section2 = this.$refs.section2
+
+    gsap.fromTo(
+      section2,
+      { clipPath: 'circle(0%)' },
+      {
+        clipPath: 'circle(100%)',
+        ease: 'Power2.easeInOut',
+        scrollTrigger: {
+          trigger: section2,
+          start: 'top top+=25%',
+          end: 'top top',
+          scrub: true,
+        },
+      }
+    );
+
+    gsap.fromTo(
+      section2,
+      { clipPath: 'circle(100%)' },
+      {
+        clipPath: 'circle(0%)',
+        ease: 'Power2.easeInOut',
+        scrollTrigger: {
+          trigger: section2,
+          start: 'bottom center+=25%',
+          end: 'bottom center',
+          scrub: true,
+        },
+      }
+    );
+
+    gsap.set(section2, {clipPath: 'circle(0%)'});
+  },
 }
 </script>
