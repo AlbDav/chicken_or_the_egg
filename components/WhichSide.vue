17bd018f7b07e73254b155fc1c842ed9cf9e9bab
Davide Albano <davidealbano96@gmail.com>
2022-02-06 20:36:42 +0100

    added whichSide section

---
+++ components/WhichSide.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    ref="whichSide"
+    class="h-screen h-screen w-full flex flex-col justify-evenly items-center"
+  >
+    <div ref="which" class="text-white text-8xl text-center">Which side</div>
+    <div ref="hr" class="w-10/12 h-1 bg-white rounded-full" />
+    <div ref="side" class="text-white text-8xl text-center">are you on?</div>
+  </div>
+</template>
+
+<script>
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+export default {
+  mounted() {
+    gsap.registerPlugin(ScrollTrigger)
+
+    const whichSide = this.$refs.whichSide
+    const which = this.$refs.which
+    const hr = this.$refs.hr
+    const side = this.$refs.side
+
+    const entranceTimeline = gsap.timeline({
+      ease: 'Power2.easeIn',
+      scrollTrigger: {
+        trigger: whichSide,
+        start: 'top top+=25%',
+        end: 'top top',
+        scrub: true,
+      },
+    })
+
+    entranceTimeline
+      .from(which, { x: '-50%', autoAlpha: 0 })
+      .from(hr, { width: '0px' }, 0)
+      .from(side, { x: '50%', autoAlpha: 0 }, 0)
+
+    const exitTimeline = gsap.timeline({
+      ease: 'Power2.easeOut',
+      scrollTrigger: {
+        trigger: whichSide,
+        start: 'bottom center+=35%',
+        end: 'bottom center',
+        scrub: true,
+      },
+    })
+
+    exitTimeline
+      .to(which, { x: '+50%', autoAlpha: 0 })
+      .to(hr, { width: '0px' }, 0)
+      .to(side, { x: '-50%', autoAlpha: 0 }, 0)
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -15,53 +15,14 @@
         <img class="h-1/2" src="../static/Saly-11.png" alt="rocket">
       </div>
     </div>
-    <div ref="section2" class="h-screen w-full bg-white">ciao</div>
+    <WhichSide />
+    <AboutCollections />
     <div class="h-screen w-full"></div>
   </div>
 </template>
 
 <script>
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-
 export default {
   name: 'IndexPage',
-  mounted() {
-    gsap.registerPlugin(ScrollTrigger)
-
-    const section2 = this.$refs.section2
-
-    gsap.fromTo(
-      section2,
-      { clipPath: 'circle(0%)' },
-      {
-        clipPath: 'circle(100%)',
-        ease: 'Power2.easeInOut',
-        scrollTrigger: {
-          trigger: section2,
-          start: 'top top+=25%',
-          end: 'top top',
-          scrub: true,
-        },
-      }
-    )
-
-    gsap.fromTo(
-      section2,
-      { clipPath: 'circle(100%)' },
-      {
-        clipPath: 'circle(0%)',
-        ease: 'Power2.easeInOut',
-        scrollTrigger: {
-          trigger: section2,
-          start: 'bottom center+=25%',
-          end: 'bottom center',
-          scrub: true,
-        },
-      }
-    )
-
-    gsap.set(section2, { clipPath: 'circle(0%)' })
-  },
 }
 </script>
